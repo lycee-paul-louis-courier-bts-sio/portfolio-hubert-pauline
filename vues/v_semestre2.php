771c08626deee31401ccd316dc91049cb29f157f
HUBERT <pauline.hubert@btssio.lan>
2026-05-07 10:47:07 +0200

    index commit

--- vues/v_semestre2.php
+++ vues/v_semestre2.php
@@ -10,7 +10,7 @@
                     <br>
                     Pour cela, nous avons modifié la base de données, puis nous avons modifié le code du site en lui-même.
                   Trello, mariabd,jquery,php,html
-                  <img src="assets/images/infodev/Logo_Trello.svg" alt="logo trello" width="50">
+                  <img src="assets/images/infodev/Logo_Trello.svg.png" alt="logo trello" width="50">
                   <img src="assets/images/infodev/mariaDB.png" alt="logo mariaDB" width="50">
                   <img src="assets/images/infodev/jquery.png" alt="logo jquery" width="50">
                   <img src="assets/images/infodev/php.jfif" alt="logo php" width="50">
