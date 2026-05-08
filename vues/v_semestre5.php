fef4a2fe152f34d15650156927162fff7ebc10f2
paulineh22 <pauline.hubert39@gmail.com>
2026-05-08 14:57:41 +0200

    ajout des lien pour l'app 4,5

--- vues/v_semestre5.php
+++ vues/v_semestre5.php
@@ -3,7 +3,7 @@
   <div class="text-primary-emphasis">
       <div class="card" >
         <div class="card-body">
-          <img src="assets/images/epokamobile/get2.PNG" class="card-img-top" alt="...">
+          <img src="assets/images/epoka/logo.png" class="card-img-top" alt="logo epoka">
           <p class="card-text">Epoka Presse, une entreprise de presse imaginaire, nécessite une application mobile Android pour la gestion de ses abonnés. J'ai réalisé ce projet en solo.
 Pour débuter, j'ai conçu une application de genre Web Service exploitant SQL Server. Elle devait présenter les détails d'un abonné ainsi que ses souscriptions, en se basant sur son code et son mot de passe.
 J'ai conçu l'application mobile Android en utilisant le logiciel Android Studio.
@@ -25,7 +25,7 @@ J'ai conçu l'application mobile Android en utilisant le logiciel Android Studio
         <div class="card project-card">
           <div class="card-body">
             <h5 class="card-title"> ...</h5>
-            <img src="assets/images/epokamobile/ get2.PNG" class="card-img-top" alt="...">
+            <img src="assets/images/epokamobile/get2.PNG" class="card-img-top" alt="...">
           </div>
         </div>
       </div>  
