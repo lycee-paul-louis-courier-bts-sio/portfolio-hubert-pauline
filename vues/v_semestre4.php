2b16a4723359d3a53bdd42321720e96fca2a4e54
paulineh22 <pauline.hubert39@gmail.com>
2026-05-08 14:00:17 +0200

    remplissage des textes  pour l'app 4

--- vues/v_semestre4.php
+++ vues/v_semestre4.php
@@ -4,7 +4,12 @@
       <div class="card" >
         <div class="card-body">
           <img src="assets/images/..." class="card-img-top" alt="...">
-          <p class="card-text">glasfish, hibernate,orm,librairie, JSP</p>
+          <p class="card-text"> Une base de données "bd_epoka_abo" a était mise en place pour la gestion des abonnements, cela n'a donc pas besoin d'être crée par Hibernate vu que celle ci est déjà existante. Nous du néanmoins modifier les classes métiers "libEpokaAbo". Le responsable des abonnements souhaite la liste des abonnements non renouvelés et ceux dont l'échéance est à moins de 30jours. glasfish, hibernate,orm,librairie, JSP
+            <img src="assets/images/epokaabo/glasfish.png" alt="logo glasfish" width="50">
+            <img src="assets/images/epokaabo/hibernate.png" alt="logo hibernate" width="50">
+            <img src="assets/images/epokaabo/JSP.png" alt="logo orm" width="50">
+            <img src="assets/images/epokaabo/orm.png" alt="logo JSP" width="50">
+          </p>
         </div>
       </div>  
     <br>    
@@ -12,62 +17,30 @@
       <div class="col">
         <div class="card project-card">
           <div class="card-body">
-            <h5 class="card-title">...</h5>
+            <h5 class="card-title">l'utilisateur choisit l'item "Abonnements orange & rouge"</h5>
             <img src="assets/images/epoka_appli/" class="card-img-top" alt="...">
+            <p class="card-text">La liste des abonnements pour la date du jour ou on  l'utilise nous répresente en orange l'abonnement a renouveler et l'accordéon en dessous nous montre les abonnements en rouge non renouvelés</p>
           </div>
         </div>
       </div>  
       <div class="col">
         <div class="card project-card">
           <div class="card-body">
-            <h5 class="card-title"> ...</h5>
+            <h5 class="card-title">l'utilisateur développe les abonnements à renouvelever</h5>
             <img src="assets/images/epoka_appli/ ..." class="card-img-top" alt="...">
+            <p class="card-text">Dans cette accordéon nous retrouvons la liste des abonnées qui peuvent renouvelés leurs abonnements ainsi que leurs informations sur l'abonnements. Nous avons également une loupe qui permet de retrouver tout les abonnements d'un abonnée</p>
           </div>
         </div>
       </div>  
       <div class="col">
         <div class="card project-card">
           <div class="card-body">
-            <h5 class="card-title">...</h5>
+            <h5 class="card-title">L'utilisateur développe les abonnements non renouvelés</h5>
             <img src="assets/images/epoka_appli/..." class="card-img-top" alt="...">
+            <p class="card-text">Dans cette accordéon nous retrouvons la liste des abonnées qui n'ont pas renouvelés leurs abonnements ainsi que leurs informations sur l'abonnements. Nous avons également une loupe qui permet de retrouver tout les abonnements d'un abonnée</p>
           </div>
         </div>
       </div>
-      <div class="col">
-        <div class="card project-card">
-          <div class="card-body">
-            <h5 class="card-title">...</h5>
-            <img src="assets/images/epoka_appli/..." class="card-img-top" alt="...">
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card project-card">
-          <div class="card-body">
-            <h5 class="card-title"> ... </h5>
-            <img src="assets/images/epoka_appli/..." class="card-img-top" alt="...">
-            <img src="assets/images/epoka_appli/..." class="card-img-top" alt="...">
-          </div>
-        </div>
-      </div>  
-      <div class="col">
-        <div class="card project-card">
-          <div class="card-body">
-            <h5 class="card-title">...</h5>
-            <img src="assets/images/epoka_appli/..." class="card-img-top" alt="...">
-            <img src="assets/images/epoka_appli/..." class="card-img-top" alt="...">
-            <img src="assets/images/epoka_appli/..." class="card-img-top" alt="...">
-          </div>
-        </div>
-      </div>    
-    </div>
-    <a href="assets/images/epoka_appli/..." download>
-      <span class="nav-icon material-symbols-rounded">... </span>
-    </a>
-    <br>
-    <a href="assets/images/epoka_appli/..." download>
-      <span class="nav-icon material-symbols-rounded">...</span>
-    </a>
   </div>
 </div>
 </section>
