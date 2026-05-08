309bdecfd0b21c85d0a7ad841f4f62c4b0b24a53
paulineh22 <pauline.hubert39@gmail.com>
2026-05-08 14:25:24 +0200

    reformulation des textes afin de mieux détailler les missions

--- vues/v_stage2025.php
+++ vues/v_stage2025.php
@@ -5,53 +5,53 @@
       <div class="col">
         <div class="card h-100">
           <div class="card-body">
-            <h5 class="card-title">Mission 1</h5>
+            <h5 class="card-title">Initiation a Ruby On Rails</h5>
             <img src="assets/images/stage2025/mission 1/accueil.PNG" class="card-img-top" alt="...">
             <img src="assets/images/stage2025/mission 1/articles.PNG" class="card-img-top" alt="...">
             <img src="assets/images/stage2025/mission 1/categorie.PNG" class="card-img-top" alt="...">
-            <p class="card-text">Initiation a Ruby On Rails</p>
+            <p class="card-text">Cela était une formation avant de commencer a pouvoior réaliser les tâches demandés</p>
           </div>
         </div>
       </div>  
       <div class="col">
         <div class="card h-100">
           <div class="card-body">
-            <h5 class="card-title">Mission 2</h5>
+            <h5 class="card-title">Amélioration d'un outil de recherche</h5>
             <img src="assets/images/stage2025/mission 2/page apres  avoir effectuer de recherche.PNG" class="card-img-top" alt="...">
             <img src="assets/images/stage2025/mission 2/page apres  avoir effectuer de recherche1.PNG" class="card-img-top" alt="...">
             <img src="assets/images/stage2025/mission 2/page sans avoir effectuer de recherche.PNG" class="card-img-top" alt="...">
-            <p class="card-text">Création d'un nouvel accordéon</p>
+            <p class="card-text">Pour cette tâche, il était nécessaire d'adapter l'outil de recherche pour faciliter la localisation des agences.</p>
           </div>
         </div>
       </div>
       <div class="col">
         <div class="card h-100">
           <div class="card-body">
-            <h5 class="card-title">Mission 3</h5>
+            <h5 class="card-title">Amélioration de la page de Création de newsletters</h5>
             <img src="assets/images/stage2025/mission 3/2025-05-BO-Stagiaires-News-Crea-Accordeons.jpg" class="card-img-top" alt="...">
             <img src="assets/images/stage2025/mission 3/2025-05-BO-Stagiaires-News-Crea-AVANT.jpg" class="card-img-top" alt="...">
             <img src="assets/images/stage2025/mission 3/2025-05-BO-Stagiaires-News-Crea-APRES.jpg" class="card-img-top" alt="...">
-            <p class="card-text">La remaniassions de la page NEWS Création pour que les boutons soient maintenant en accordéon et que son contenu soit dedans </p>
+            <p class="card-text">Ici, il était nécessaire de modifier la façon dont les newsletters sont modifiées. Actuellement, il est possible de personnaliser l'en-tête et le pied de page par défaut d'une part, tout en modifiant une autre newsletter qui peut posséder son propre en-tête et pied de page d'autre part.</p>
           </div>
         </div>
       </div>
       <div class="col">
         <div class="card h-100">
           <div class="card-body">
-            <h5 class="card-title">Mission 4</h5>
+            <h5 class="card-title">Réorganisation de la page de Mise en page</h5>
             <img src="assets/images/stage2025/mission 4/BO-THEMES-Entete-Avant.jpg" class="card-img-top" alt="...">
             <img src="assets/images/stage2025/mission 4/BO-THEMES-Entete-Apres.jpg" class="card-img-top" alt="...">
-            <p class="card-text">Modification de la page afin qu’elle ressemble à l’après demandé toujours dans leurs anciens sites</p>
+            <p class="card-text">L'objectif de cette mission était d'améliorer la fonctionnalité de cette page. L'objectif est d'arranger les divers widgets sur les différentes pages du site web du client.</p>
           </div>
         </div>
       </div>
       <div class="col">
         <div class="card h-100">
           <div class="card-body">
-            <h5 class="card-title">Mission 5</h5>
+            <h5 class="card-title">Création d'un widget Carrousel d'avis</h5>
             <img src="assets/images/stage2025/mission 5/Widget-Avis-BO.png" class="card-img-top" alt="...">
             <img src="assets/images/stage2025/mission 5/Widget-Avis-FO.png" class="card-img-top" alt="...">
-            <p class="card-text">Création de widget</p>
+            <p class="card-text">Je n'ai pas eu la possibilité de conclure cette tâche, étant donné que le stage arrivait à son terme. Elle impliquait la conception d'un widget pour le système de gestion de contenu de l'entreprise, permettant aux clients d'afficher les avis sur leur propre site web.</p>
           </div>
         </div>
       </div> 
--- vues/v_stage2026.php
+++ vues/v_stage2026.php
@@ -5,18 +5,18 @@
       <div class="col">
         <div class="card h-100">
           <div class="card-body">
-            <h5 class="card-title">Mission 1</h5>
+            <h5 class="card-title">Création de la page statistique de production</h5>
             <img src="assets/images/stage2026/mission1/avec erreur.PNG" class="card-img-top" alt="...">
             <img src="assets/images/stage2026/mission1/structure de la table stat_production.PNG" class="card-img-top" alt="...">
             <img src="assets/images/stage2026/mission1/tableau sans alerte.PNG" class="card-img-top" alt="...">
-            <p class="card-text">Création de la page statistique de production</p>
+            <p class="card-text">Ces chiffres fournissent une explication quant aux raisons pour lesquelles certains produits sont invalidés.</p>
           </div>
         </div>
       </div>  
       <div class="col">
         <div class="card h-100">
           <div class="card-body">
-            <h5 class="card-title">Mission 2</h5>
+            <h5 class="card-title">Création de l'accordéon statistique d'indexation</h5>
             <img src="assets/images/stage2026/mission2/ajout lien.PNG" class="card-img-top" alt="...">
             <img src="assets/images/stage2026/mission2/fini de stats_indexation.PNG" class="card-img-top" alt="...">
             <img src="assets/images/stage2026/mission2/mapping lieux.PNG" class="card-img-top" alt="...">
@@ -25,21 +25,21 @@
             <img src="assets/images/stage2026/mission2/stats indexation avec alerte.PNG" class="card-img-top" alt="...">
             <img src="assets/images/stage2026/mission2/bd de produit voyages indexation stat indexation.PNG" class="card-img-top" alt="...">
             <img src="assets/images/stage2026/mission2/dans mapping stats indexation avec import rajouter.PNG" class="card-img-top" alt="...">            
-            <p class="card-text">Création de l'accordéon statistique d'indexation</p>
+            <p class="card-text">Ces données expliquent pourquoi certains articles ont été éliminés pendant le processus d'indexation. L'indexation implique l'importation de toutes les informations fournies par les clients et leur conversion en une norme unique.</p>
           </div>
         </div>
       </div>
       <div class="col">
         <div class="card h-100">
           <div class="card-body">
-            <h5 class="card-title">Mission 3</h5>
+            <h5 class="card-title">Création de l'accordéon statistique des tarifs</h5>
             <img src="assets/images/stage2026/mission3/diponibilite.PNG" class="card-img-top" alt="...">
             <img src="assets/images/stage2026/mission3/formule page.PNG" class="card-img-top" alt="...">
             <img src="assets/images/stage2026/mission3/lien pour annexe 8.PNG" class="card-img-top" alt="...">
             <img src="assets/images/stage2026/mission3/lieux.PNG" class="card-img-top" alt="...">
             <img src="assets/images/stage2026/mission3/page stats indexation avec alerte.PNG" class="card-img-top" alt="...">
             <img src="assets/images/stage2026/mission3/stats avec popover.PNG" class="card-img-top" alt="...">
-            <p class="card-text">Création de l'accordéon statistique des tarifs</p>
+            <p class="card-text">Ces statistiques se comparent à l'indexation du catalogue, la distinction étant que nous parlons ici de l'indexation des prix. Ceci nécessite l'utilisation d'autres bases de données, et par conséquent, l'élaboration de nouvelles procédures et tables. Toutefois, la présentation des statistiques sera effectuée sur la même page que celle de l'indexation du catalogue.</p>
           </div>
         </div>
       </div>     
