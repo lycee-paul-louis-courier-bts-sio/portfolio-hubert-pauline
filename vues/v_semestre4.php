a1b211148d8c7da9369c63bd3db06da7a8d5e79d
paulineh22 <pauline.hubert39@gmail.com>
2026-05-08 14:41:35 +0200

    ajout des images app 4 et5

--- vues/v_semestre4.php
+++ vues/v_semestre4.php
@@ -3,7 +3,7 @@
   <div class="text-primary-emphasis">
       <div class="card" >
         <div class="card-body">
-          <img src="assets/images/..." class="card-img-top" alt="...">
+          <img src="assets/images/accueil.PNG" class="card-img-top" alt="liste de tout les abonnements">
           <p class="card-text"> Une base de données "bd_epoka_abo" a était mise en place pour la gestion des abonnements, cela n'a donc pas besoin d'être crée par Hibernate vu que celle ci est déjà existante. Nous du néanmoins modifier les classes métiers "libEpokaAbo". Le responsable des abonnements souhaite la liste des abonnements non renouvelés et ceux dont l'échéance est à moins de 30jours. glasfish, hibernate,orm,librairie, JSP
             <img src="assets/images/epokaabo/glasfish.png" alt="logo glasfish" width="50">
             <img src="assets/images/epokaabo/hibernate.png" alt="logo hibernate" width="50">
@@ -18,7 +18,7 @@
         <div class="card project-card">
           <div class="card-body">
             <h5 class="card-title">l'utilisateur choisit l'item "Abonnements orange & rouge"</h5>
-            <img src="assets/images/epoka_appli/" class="card-img-top" alt="...">
+            <img src="assets/images/epoka_appli/accueil.PNG" class="card-img-top" alt="liste abonnements">
             <p class="card-text">La liste des abonnements pour la date du jour ou on  l'utilise nous répresente en orange l'abonnement a renouveler et l'accordéon en dessous nous montre les abonnements en rouge non renouvelés</p>
           </div>
         </div>
@@ -27,7 +27,7 @@
         <div class="card project-card">
           <div class="card-body">
             <h5 class="card-title">l'utilisateur développe les abonnements à renouvelever</h5>
-            <img src="assets/images/epoka_appli/ ..." class="card-img-top" alt="...">
+            <img src="assets/images/epoka_appli/ orange.PNG" class="card-img-top" alt="liste à renouveler">
             <p class="card-text">Dans cette accordéon nous retrouvons la liste des abonnées qui peuvent renouvelés leurs abonnements ainsi que leurs informations sur l'abonnements. Nous avons également une loupe qui permet de retrouver tout les abonnements d'un abonnée</p>
           </div>
         </div>
@@ -36,7 +36,7 @@
         <div class="card project-card">
           <div class="card-body">
             <h5 class="card-title">L'utilisateur développe les abonnements non renouvelés</h5>
-            <img src="assets/images/epoka_appli/..." class="card-img-top" alt="...">
+            <img src="assets/images/epoka_appli/rouge.PNG" class="card-img-top" alt="liste non renouveler">
             <p class="card-text">Dans cette accordéon nous retrouvons la liste des abonnées qui n'ont pas renouvelés leurs abonnements ainsi que leurs informations sur l'abonnements. Nous avons également une loupe qui permet de retrouver tout les abonnements d'un abonnée</p>
           </div>
         </div>
