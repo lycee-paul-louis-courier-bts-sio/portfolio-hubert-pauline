635b908a489260e4c6effdbfdd5bbbc8bddfbbf7
paulineh22 <pauline.hubert39@gmail.com>
2026-05-14 19:27:33 +0200

    grandissement des images du stage

--- vues/v_stage2025.php
+++ vues/v_stage2025.php
@@ -24,13 +24,9 @@ Cette expérience a été très enrichissante. Elle m'a permis de me familiarise
         <div class="card h-100">
           <div class="card-body">
             <h5 class="card-title">Initiation a Ruby On Rails</h5>
-            <img src="assets/images/stage2025/mission 1/accueil.PNG" 
-     class="card-img-top" 
-     style="transition: transform 0.3s; cursor: pointer;" 
-     onmouseover="this.style.transform='scale(1.5)'; this.style.zIndex='100'; this.style.position='relative';" 
-     onmouseout="this.style.transform='scale(1)'; this.style.zIndex='1';">
-            <img src="assets/images/stage2025/mission 1/articles.PNG" class="card-img-top" alt="...">
-            <img src="assets/images/stage2025/mission 1/categorie.PNG" class="card-img-top" alt="...">
+            <img src="assets/images/stage2025/mission 1/accueil.PNG" class="card-img-top" style="transition: transform 0.3s; cursor: pointer;" onmouseover="this.style.transform='scale(1.5)'; this.style.zIndex='100'; this.style.position='relative';" onmouseout="this.style.transform='scale(1)'; this.style.zIndex='1';">
+            <img src="assets/images/stage2025/mission 1/articles.PNG" class="card-img-top" style="transition: transform 0.3s; cursor: pointer;" onmouseover="this.style.transform='scale(1.5)'; this.style.zIndex='100'; this.style.position='relative';" onmouseout="this.style.transform='scale(1)'; this.style.zIndex='1';">
+            <img src="assets/images/stage2025/mission 1/categorie.PNG" class="card-img-top" style="transition: transform 0.3s; cursor: pointer;" onmouseover="this.style.transform='scale(1.5)'; this.style.zIndex='100'; this.style.position='relative';" onmouseout="this.style.transform='scale(1)'; this.style.zIndex='1';">
             <p class="card-text">Cela était une formation avant de commencer a pouvoior réaliser les tâches demandés</p>
           </div>
         </div>
@@ -39,9 +35,9 @@ Cette expérience a été très enrichissante. Elle m'a permis de me familiarise
         <div class="card h-100">
           <div class="card-body">
             <h5 class="card-title">Amélioration d'un outil de recherche</h5>
-            <img src="assets/images/stage2025/mission 2/page apres  avoir effectuer de recherche.PNG" class="card-img-top" alt="...">
-            <img src="assets/images/stage2025/mission 2/page apres  avoir effectuer de recherche1.PNG" class="card-img-top" alt="...">
-            <img src="assets/images/stage2025/mission 2/page sans avoir effectuer de recherche.PNG" class="card-img-top" alt="...">
+            <img src="assets/images/stage2025/mission 2/page apres  avoir effectuer de recherche.PNG" class="card-img-top" style="transition: transform 0.3s; cursor: pointer;" onmouseover="this.style.transform='scale(1.5)'; this.style.zIndex='100'; this.style.position='relative';" onmouseout="this.style.transform='scale(1)'; this.style.zIndex='1';">
+            <img src="assets/images/stage2025/mission 2/page apres  avoir effectuer de recherche1.PNG" class="card-img-top" style="transition: transform 0.3s; cursor: pointer;" onmouseover="this.style.transform='scale(1.5)'; this.style.zIndex='100'; this.style.position='relative';" onmouseout="this.style.transform='scale(1)'; this.style.zIndex='1';">
+            <img src="assets/images/stage2025/mission 2/page sans avoir effectuer de recherche.PNG" class="card-img-top" style="transition: transform 0.3s; cursor: pointer;" onmouseover="this.style.transform='scale(1.5)'; this.style.zIndex='100'; this.style.position='relative';" onmouseout="this.style.transform='scale(1)'; this.style.zIndex='1';">
             <p class="card-text">Pour cette tâche, il était nécessaire d'adapter l'outil de recherche pour faciliter la localisation des agences.</p>
           </div>
         </div>
@@ -50,9 +46,9 @@ Cette expérience a été très enrichissante. Elle m'a permis de me familiarise
         <div class="card h-100">
           <div class="card-body">
             <h5 class="card-title">Amélioration de la page de Création de newsletters</h5>
-            <img src="assets/images/stage2025/mission 3/2025-05-BO-Stagiaires-News-Crea-Accordeons.jpg" class="card-img-top" alt="...">
-            <img src="assets/images/stage2025/mission 3/2025-05-BO-Stagiaires-News-Crea-AVANT.jpg" class="card-img-top" alt="...">
-            <img src="assets/images/stage2025/mission 3/2025-05-BO-Stagiaires-News-Crea-APRES.jpg" class="card-img-top" alt="...">
+            <img src="assets/images/stage2025/mission 3/2025-05-BO-Stagiaires-News-Crea-Accordeons.jpg" class="card-img-top" style="transition: transform 0.3s; cursor: pointer;" onmouseover="this.style.transform='scale(1.5)'; this.style.zIndex='100'; this.style.position='relative';" onmouseout="this.style.transform='scale(1)'; this.style.zIndex='1';">
+            <img src="assets/images/stage2025/mission 3/2025-05-BO-Stagiaires-News-Crea-AVANT.jpg" class="card-img-top" style="transition: transform 0.3s; cursor: pointer;" onmouseover="this.style.transform='scale(1.5)'; this.style.zIndex='100'; this.style.position='relative';" onmouseout="this.style.transform='scale(1)'; this.style.zIndex='1';">
+            <img src="assets/images/stage2025/mission 3/2025-05-BO-Stagiaires-News-Crea-APRES.jpg" class="card-img-top" style="transition: transform 0.3s; cursor: pointer;" onmouseover="this.style.transform='scale(1.5)'; this.style.zIndex='100'; this.style.position='relative';" onmouseout="this.style.transform='scale(1)'; this.style.zIndex='1';">
             <p class="card-text">Ici, il était nécessaire de modifier la façon dont les newsletters sont modifiées. Actuellement, il est possible de personnaliser l'en-tête et le pied de page par défaut d'une part, tout en modifiant une autre newsletter qui peut posséder son propre en-tête et pied de page d'autre part.</p>
           </div>
         </div>
@@ -61,8 +57,8 @@ Cette expérience a été très enrichissante. Elle m'a permis de me familiarise
         <div class="card h-100">
           <div class="card-body">
             <h5 class="card-title">Réorganisation de la page de Mise en page</h5>
-            <img src="assets/images/stage2025/mission 4/BO-THEMES-Entete-Avant.jpg" class="card-img-top" alt="...">
-            <img src="assets/images/stage2025/mission 4/BO-THEMES-Entete-Apres.jpg" class="card-img-top" alt="...">
+            <img src="assets/images/stage2025/mission 4/BO-THEMES-Entete-Avant.jpg" class="card-img-top" style="transition: transform 0.3s; cursor: pointer;" onmouseover="this.style.transform='scale(1.5)'; this.style.zIndex='100'; this.style.position='relative';" onmouseout="this.style.transform='scale(1)'; this.style.zIndex='1';">
+            <img src="assets/images/stage2025/mission 4/BO-THEMES-Entete-Apres.jpg" class="card-img-top" style="transition: transform 0.3s; cursor: pointer;" onmouseover="this.style.transform='scale(1.5)'; this.style.zIndex='100'; this.style.position='relative';" onmouseout="this.style.transform='scale(1)'; this.style.zIndex='1';">
             <p class="card-text">L'objectif de cette mission était d'améliorer la fonctionnalité de cette page. L'objectif est d'arranger les divers widgets sur les différentes pages du site web du client.</p>
           </div>
         </div>
@@ -71,8 +67,8 @@ Cette expérience a été très enrichissante. Elle m'a permis de me familiarise
         <div class="card h-100">
           <div class="card-body">
             <h5 class="card-title">Création d'un widget Carrousel d'avis</h5>
-            <img src="assets/images/stage2025/mission 5/Widget-Avis-BO.png" class="card-img-top" alt="...">
-            <img src="assets/images/stage2025/mission 5/Widget-Avis-FO.png" class="card-img-top" alt="...">
+            <img src="assets/images/stage2025/mission 5/Widget-Avis-BO.png" class="card-img-top" style="transition: transform 0.3s; cursor: pointer;" onmouseover="this.style.transform='scale(1.5)'; this.style.zIndex='100'; this.style.position='relative';" onmouseout="this.style.transform='scale(1)'; this.style.zIndex='1';">
+            <img src="assets/images/stage2025/mission 5/Widget-Avis-FO.png" class="card-img-top" style="transition: transform 0.3s; cursor: pointer;" onmouseover="this.style.transform='scale(1.5)'; this.style.zIndex='100'; this.style.position='relative';" onmouseout="this.style.transform='scale(1)'; this.style.zIndex='1';">
             <p class="card-text">Je n'ai pas eu la possibilité de conclure cette tâche, étant donné que le stage arrivait à son terme. Elle impliquait la conception d'un widget pour le système de gestion de contenu de l'entreprise, permettant aux clients d'afficher les avis sur leur propre site web.</p>
           </div>
         </div>
